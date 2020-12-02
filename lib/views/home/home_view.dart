85ce1ab42d380057759f6e85617d33ce37c94cbc
pebbies <63240836+pebbies@users.noreply.github.com>
2020-12-02 17:15:37 -0500

    phase 1 -c1

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:gen2/widgets/navigation_bar/navigation_bar.dart';
 
 class HomeView extends StatelessWidget {
+  const HomeView({Key key}) : super(key: key);
+
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-
-      ),
-      home: HomeView()
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(children: <Widget>[
+        NavigationBar()
+      ])
     );
   }
 }
